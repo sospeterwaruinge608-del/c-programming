84cdedfa8769acc9045c7ea38c3dc6a38a3e90e4
sospeterwaruinge608-del <sospeterwaruinge608@gmail.com>
2026-09-19 14:49:05 +0300

    Add files via upload

---
+++ SEMESTER UNITS.c
@@ -0,0 +1,23 @@
+// program display semester units
+
+/*name:Sospeter Gikonyo Waruinge
+Reg no:CT100/G/30691/26
+description: C display semester units
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Units offered this semester:\n");
+	
+	printf("1. analogue electronics\n");
+	printf("2. essentials of accounting\n");
+	printf("3. mathematics for science\n");
+	printf("4. computer system amd organizations\n");
+	printf("5. introduction to vomputer applications\n");
+	printf("6. fundamentals of digital technology and artificial inteligence\n");
+	printf("7. introduction to programming and algorithms\n");
+	printf("8. communication and information literacy skills\n");
+	return 0;
+}
